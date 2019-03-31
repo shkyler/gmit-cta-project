0f5cf6fff32a7f0de9d4abb667958bceca89bbdf
Patrick Moore <moorepa@outlook.com>
2019-03-31 20:56:19 +0100

    fixed the plotting function

--- project.py
+++ project.py
@@ -10,16 +10,16 @@ import matplotlib.pyplot as plt
   the algorithms easier to replicate
 '''
 # create a function to be used for generating the test arrays
-def arrayCreate(size):
+def array_create(size):
   # seed the RNG
-  np.random.seed(100)
+  np.random.seed(150)
   # return the array of the selected size
   return [np.random.randint(1000) for i in range(size)]
 
 
 # Section 2 - Define the 5 Sorting Algorithms to be Used
 # define the bubblesort algorithm - http://interactivepython.org/courselib/static/pythonds/SortSearch/TheInsertionSort.html
-def bubble(alist):
+def bubble_sort(alist):
     # outer for-loop runs from the last item in the list to the first in steps of 1
     for passnum in range(len(alist)-1,0,-1):
         # inner for-loop runs from the start of the list to the current value of passnum
@@ -33,7 +33,7 @@ def bubble(alist):
                 
 # define the insertion sort algorithm http://interactivepython.org/runestone/static/pythonds/SortSearch/TheBubbleSort.html
 # note this needs comments
-def insertion(alist):
+def insertion_sort(alist):
    for index in range(1,len(alist)):
 
      currentvalue = alist[index]
@@ -48,9 +48,9 @@ def insertion(alist):
 # Section 3 - Define the timer functions to be used
 
 # define a function that takes an array and a sorting algorithm and times how long it takes to run
-def timer(a,sort_algo):
+def timer(input_array,sort_algo):
   start = time.time()
-  sort_algo(a)
+  sort_algo(input_array)
   end = time.time()
   return (end-start)
 
@@ -69,17 +69,17 @@ def average_time(runs, data, sort_algo):
 # Section 4 - Formatting the output
 
 # create a function to carry out the trials
-def algoTrial(algo, testSize):
+def algo_trial(algo, test_size):
   # this function takes an algorithm and test size and returns the average result of 10 trials in ms - formatted to 3 decimal places
-  return float("{:10.3f}".format(average_time(10,arrayCreate(testSize),algo)*1000))
+  return float("{:10.3f}".format(average_time(10,array_create(test_size),algo)*1000))
 
 # create a column creator function
 # pass a list of sorting algorithms and a test size
-def colCreate(algos, testSize):  
+def col_create(algos, test_size):  
   # the algorithm will test each algorthim for a given test size ..
   col = []
   for i in algos:
-    col.append(algoTrial(i, testSize))
+    col.append(algo_trial(i, test_size))
   # .. return a list of results  
   return(col)  
 
@@ -87,15 +87,18 @@ def colCreate(algos, testSize):
 # 'sorts' is used to index the data frame
 sorts = ['Bubble Sort', 'Insertion Sort']
 # 'algorithms' is a list of the function names
-algorithms = [bubble, insertion]
-
+n = [100,250,500,750, 1000, 1250, 2500, 3750, 5000, 6250, 7500, 8750, 10000]
+algorithms = [bubble_sort, insertion_sort]
 # create an empty pandas data frame to store the data
-data = pd.DataFrame({"size":sorts, "100":colCreate(algorithms,100), "250":colCreate(algorithms,250),"500":colCreate(algorithms,500),"750":colCreate(algorithms,750), "1000":colCreate(algorithms,1000)})
-print(data.to_string(index=False))
+data = pd.DataFrame({"size":sorts, "100":col_create(algorithms,100), "250":col_create(algorithms,250),"500":col_create(algorithms,500),"750":col_create(algorithms,750), "1000":col_create(algorithms,1000),"1250":col_create(algorithms,1250),"2500":col_create(algorithms,2500), "3750":col_create(algorithms,3750), "5000":col_create(algorithms,5000), "6250":col_create(algorithms,6250), "7500":col_create(algorithms,7500), "8750":col_create(algorithms,8750), "10000":col_create(algorithms,10000)})
+data.set_index("size",inplace=True)
+print(data.to_string())
 
-#print(data[0,1:5])
-#plt.plot(n,data.loc[:,['Bubble Sort']])
-#print("{:10.3f}".format(average_time(10,arr1250,bubble)*1000))
+for i in range(2):
+  plt.plot(n,data.iloc[i])
+
+plt.show()
+
 
 
 # Section 5 - User Interface
